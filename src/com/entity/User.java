0c74acca45ce2e5d04772ac6a0b053238e5f5ebe
chenlihao123 <821865057@qq.com>
2020-12-07 17:22:31 +0800

    陈立豪第一次修改

--- src/com/entity/User.java
+++ src/com/entity/User.java
@@ -6,9 +6,18 @@ package com.entity;
  */
 public class User {
     private String username;//这是陈立豪添加的username
+    private String password;//这是陈立豪第二次添加的password
 
     public String getUsername() {
         return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public void setUsername(String username) {
